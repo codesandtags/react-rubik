af20bbc5349c50c3c27b94a5514993ef9ab74daf
Edwin Torres <codesandtags@gmail.com>
2017-04-02 21:04:23 -0500

    feat: Added highlightjs plugin to show code :D

--- src/container/ComponentList/index.js
+++ src/container/ComponentList/index.js
@@ -2,14 +2,35 @@
 import React from "react";
 // Components
 import Counter from "./../../components/Counter/Counter";
+// JS Libraries
+import hljs from "highlightjs/highlight.pack.min";
+// Styles
+import "highlightjs/styles/tomorrow-night-eighties.css";
 
 export default class ComponentList extends React.Component {
+  componentDidMount() {
+    const $blocks = document.querySelectorAll('pre code');
+    $blocks.forEach((block) => {
+      console.log(block);
+      hljs.highlightBlock(block);
+    });
+  }
 
   render() {
     return (
       <section>
-        <Counter startNumber="5"/>
-        <Counter startNumber="250" iconText="❤️"/>
+        <article>
+          <h2>Counter</h2>
+          <Counter startNumber="5"/>
+          <Counter startNumber="250" iconText="❤️"/>
+          <h3>Code</h3>
+          <pre>
+              <code className="hljs html">
+                &lt;Counter startNumber=&quot;5&quot;/&gt;<br/>
+                &lt;Counter startNumber=&quot;250&quot; iconText=&quot;❤️&quot;/&gt;
+              </code>
+          </pre>
+        </article>
       </section>
     )
   }
